ea4185d6a63a448aed91495b98a49f7a7540cbcf
ericGOD24 <ericobandeira@hotmail.com>
2018-04-24 08:39:39 -0300

    adicionar comentários ao projeto

--- LeProjetoDePapel.c
+++ LeProjetoDePapel.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//1 - Abre arquivo e armazena em uma matriz o valor de cada pixel da imagem
+//2 - Função que rotacione um vetor, pegue o ultimo elemento do vetor e coloque como o primeiro elemento
+//3 - Função que use a função de rotacionar para rotacionar-lo até voltar ao vetor inicial, e comparar o 
+//valor do vetor em decimal e armazenar o menor desses valores
+//4 - Função que transforme um vetor de numeros binarios em um numero decimal
+//5 - Cada pixel (8) deve ser comparado com seus vizinhos
+//6 - Função que compare os pixels vizinhos e retorna uma matriz em que cada elemento será 0 ou 1, 
+//onde 0 será se o valor do elemento correspondente for menor do que a média
+//7 - Função que faça a media do valor dos elementos da matriz que representa um pixel
+//8 - Função que faça a formula de contraste
+//9 - Função que faça a formula de energia
+//10 - Função que faça a formula de homogenidade
+
 int main (int argc, char *argv[]){
+    
 return 0;
 }
