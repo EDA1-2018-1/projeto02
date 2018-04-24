d84e66f1021ea03de6b51f07280dab579faca427
Max <maxhb.df@gmail.com>
2018-04-24 06:46:34 -0300

    [ADD] more coment... for alloc

--- LeProjetoDePapel.c
+++ LeProjetoDePapel.c
@@ -3,12 +3,28 @@
 #define MAX 10
 
 //1 - Abre arquivo e armazena em uma matriz o valor de cada pixel da imagem
+int LerImagem (){
+  FILE *fp;
+  int i = 0, j = 0, n;
+
+    while (!EOF) {
+        char asphalt [] = "./DataSet/asphalt/asphalt_'%d'.txt", n;
+
+        fp = fopen(asphalt, "r");
+
+        n++;
+    }
+}
+
+
+
+
 //2 - Função que rotacione um vetor, pegue o ultimo elemento do vetor e coloque como o primeiro elemento
-//3 - Função que use a função de rotacionar para rotacionar-lo até voltar ao vetor inicial, e comparar o 
+//3 - Função que use a função de rotacionar para rotacionar-lo até voltar ao vetor inicial, e comparar o
 //valor do vetor em decimal e armazenar o menor desses valores
 //4 - Função que transforme um vetor de numeros binarios em um numero decimal
 //5 - Cada pixel (8) deve ser comparado com seus vizinhos
-//6 - Função que compare os pixels vizinhos e retorna uma matriz em que cada elemento será 0 ou 1, 
+//6 - Função que compare os pixels vizinhos e retorna uma matriz em que cada elemento será 0 ou 1,
 //onde 0 será se o valor do elemento correspondente for menor do que a média
 //7 - Função que faça a media do valor dos elementos da matriz que representa um pixel
 //8 - Função que faça a formula de contraste
@@ -17,6 +33,6 @@
 
 int main (int argc, char *argv[]){
     int salvaImagem[MAX], i, j;
-    
+
 return 0;
 }
