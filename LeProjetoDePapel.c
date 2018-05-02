fa5379a8ce3b4426b4e4c02631fe2bb082ad6a7d
ericGOD24 <ericobandeira@hotmail.com>
2018-05-02 00:04:44 -0300

    add function for test

--- LeProjetoDePapel.c
+++ LeProjetoDePapel.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-void gerarNumerosAleatorios(int, int *);
+void gerarNumerosTreinamento(int, int *);
+void gerarNumerosTeste(int *, int*);
 
 int main (int argc, char *argv[]){
+
     FILE *fp;
     char nomeArquivo[33] = "asphalt_01.txt", guardarPonto;
-    int *imagem, linha=0, coluna=0, tempVar, numAleatorios[25], random=49, cont = 0;
-    gerarNumerosAleatorios(random,numAleatorios);
+    int *imagem, linha=0, coluna=0, tempVar, numTreinamento[25];
+    int random=49, contAsphalt = 0, numTeste[25];
+
+    gerarNumerosTreinamento( random, numTreinamento);
+    gerarNumerosTeste(numTeste, numTreinamento);
+
     imagem = (int *) malloc(sizeof(int));
-    for(cont = 0; cont < 25; cont++){
-        if(numAleatorios[cont]<10){
-            sprintf(nomeArquivo,"./DataSet/asphalt/asphalt_0%d.txt", numAleatorios[cont]);
+    for(contAsphalt = 0; contAsphalt < 25; contAsphalt++){
+        if(numTreinamento[contAsphalt] < 10){
+            sprintf(nomeArquivo,"./DataSet/asphalt/asphalt_0%d.txt", numTreinamento[contAsphalt]);
         }else{
-            sprintf(nomeArquivo,"./DataSet/asphalt/asphalt_%d.txt", numAleatorios[cont]);
+            sprintf(nomeArquivo,"./DataSet/asphalt/asphalt_%d.txt", numTreinamento[contAsphalt]);
         }
         printf("%s", nomeArquivo);
         fp = fopen(nomeArquivo,"r");
@@ -29,21 +35,37 @@ int main (int argc, char *argv[]){
         fclose(fp);
         free(imagem);
     }
-    
+
+
 return 0;
 }
 
-void gerarNumerosAleatorios(int random, int *numAleatorios){
+void gerarNumerosTreinamento(int random, int *numTreinamento){
     int i, j, tempComp;
     srand((unsigned)time(NULL));
     for(i=0 ; i < 25 ; i++){
         tempComp = (rand()%random) + 1;
         for(j=0 ; j < i; j++){
-            if(tempComp == numAleatorios[j]){
+            if(tempComp == numTreinamento[j]){
                 i--;
                 break;
             }
         }
-        numAleatorios[j] = tempComp;
+        numTreinamento[j] = tempComp;
+    }        
+}
+
+void gerarNumerosTeste(int *numTeste, int *numTreinamento){
+    int i, j, a=0;
+    for(i=0 ; i < 50 ; i++){
+        for(j=0 ; j < 25; j++){
+            if(i == numTreinamento[j]){
+                break;
+            }   
+        }  
+        if(j == 25){
+            numTeste[a] = i;
+            a++;
+        }
     }        
 }
