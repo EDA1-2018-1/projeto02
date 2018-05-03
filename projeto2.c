e752dcb5c39f03e8fb66af85035ca5daced9422a
Max <maxhb.df@gmail.com>
2018-05-03 04:24:48 -0300

    então né

--- projeto2.c
+++ projeto2.c
@@ -109,9 +109,141 @@ int generatorRandomNumbers(){
   return (rand() % (50)) + 1;
 }
 
-// void ilbp(){
-//
-// }
+void ilbp(int **pixels, int rows, int columns, float **arrayImages, int value){
+  int i,j;
+  for(i = 1; i < rows-1; i++) {
+    for (j = 1; j < columns-1; j++) {
+      quadranteIlbp(pixels,i,j,arrayImages,value);
+    }
+  }
+}
+
+void quadranteIlbp(int **pixels,int row,int colum,float **arrayImages,int value){
+  float total=0.0;
+  int value=0,min;
+  int vetor[9];
+  for (int i = row-1; i <= row+1; i++) {
+    for (int j = colum-1; j <= colum+1; j++) {
+      total += *(*(pixels+i)+j);
+      vetor[value] = pixels[i][j];
+      value++;
+    }
+  }
+  for (int i = 0; i < 9; i++) {
+    if (total/9 >= vetor[i]) {
+      vetor[i] = 1;
+    } else {
+      vetor[i] = 0;
+    }
+  }
+  min = minValue(vetor,256,0);
+  if (count%2) {
+    arrayImages[(count-1)/2][min]+=1;
+  } else {
+    arrayImages[50/2+count/2][min]+=1;
+  }
+}
+
+int minValue(int *vetor,int mini,int cont){
+  int n;
+  if (count == 9) {
+    return mini;
+  } else {
+    n = binForDec(vetor);
+    if (n < mini) {
+      mini = n;
+    }
+    rotVetor(vetor);
+    return minValue(vetor,min,count+1);
+  }
+}
+
+int binForDec(int *bin){
+  int decimal=0;
+  for (int i = 0; i < 9; i++) {
+    decimal += pow(2,(8-i))*bin[i];
+  }
+  return decimal;
+}
+
+void rotVetor(int *vetor){
+  int count = vetor[0];
+  int count1;
+  vetor[0] = vetor[8];
+  for (int i = 1; i < 9; i++) {
+    count1 = vetor[i];
+    vetor[i] = count;
+    count = count1;
+  }
+}
+
+void normVet(float *vetor,int tamVet){
+
+  int maior=0,menor=10000,i;
+  for (i = 0; i < tamVet; i++) {
+    if (vetor[i] < menor) {
+      menor = vetor[i];
+    }else if(vetor[i] > maior){
+      maior = vetor[i];
+    }
+  }
+  if (maior == menor) {
+    return;
+  }
+  for (i = 0; i < tamVet; i++) {
+    vetor[i] = (vetor[i] - menor)/(maior - menor);
+  }
+}
+
+void fazMedia(float **arrayImages,float *medGrass,float *medAsphalt){
+
+  int i,j;
+  float total=0.0;
+
+  for (j = 0; j < BINMAX + 24; j++) {
+    for (i = 0; i < 50/2; i++) {
+      total+= arrayImages[i][j];
+    }
+    medGrass[j] = total/(50/2);
+    total = 0.0;
+    for (i = 50/2; i < 50; i++) {
+      total+= arrayImages[i][j];
+    }
+    medAsphalt[j] = total/(50/2);
+    total = 0.0;
+  }
+
+}
+
+void medEuclidiana(float **arrayImages,float *medAsphalt,float *medGrass,int *acerto,int *aceitacaoFalsa,int *rejeicaoFalsa){
+  float total = 0.0,distGrass,distAsphalt;
+  int i,j;
+  for (i = 0; i < 50; i++) {
+    for (j = 0; j < 536; j++) {
+      total+= pow(arrayImages[i][j] - medGrass[j],2);
+    }
+    distGrass = sqrt(total);
+    total = 0.0;
+    for (j = 0; j < 536; j++) {
+      total+= pow(arrayImages[i][j] - medAsphalt[j],2);
+    }
+    distAsphalt = sqrt(total);
+    total = 0.0;
+    if (i < 50/2) {
+      if (distGrass < distAsphalt) {
+        *acerto += 1;
+      } else {
+        *rejeicaoFalsa += 1;
+      }
+    } else{
+      if (distGrass > distAsphalt) {
+        *acerto += 1;
+      } else {
+        *aceitacaoFalsa += 1;
+      }
+    }
+  }
+}
 
 // void glcm(){
 //
